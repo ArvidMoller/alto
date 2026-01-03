506ad58798b8a693eef15d5ab48f2fae986482c0
Möllinatorn3000 <arvid.moller@elev.ga.ntig.se>
2026-01-03 20:34:01 +0100

    general updates to training.py

--- src/model_training/training.py
+++ src/model_training/training.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import datetime as dt
+import random
+import copy
 
 import torch
 
@@ -20,6 +22,24 @@ keras.mixed_precision.set_global_policy("mixed_float16")
 print("Pytorch version. ", torch.__version__, "GPU name: ", torch.cuda.get_device_name())
 
 
+# Computes shape of python list.
+#
+# Parameters:
+# arr: Multi-dimensional list.
+# dimensions: Amount of dimensions.
+#
+# Returns
+# shape: Shape of multi-dimensional list.
+def list_shape(arr, dimensions):
+    shape = []
+
+    for i in range(dimensions): 
+        shape.append(len(arr))
+        arr = arr[0]
+
+    return shape
+
+
 # Saves all image arrays in an dictionary in order to make sure they are only loaded in into RAM once. 
 # 
 # Parameters:
@@ -33,7 +53,7 @@ def get_image(path, image_cache):
     if path not in image_cache:
         img = load_img(path)
         img = img.convert("L")
-        image_cache[path] = img_to_array(img, dtype=np.uint8)
+        image_cache[path] = np.float16(img_to_array(img, dtype=np.uint8) / 255)
 
     return image_cache[path] , image_cache
 
@@ -84,16 +104,16 @@ def load_dataset(path, sequence_size, start_index, time_delta):
                 print(img_arr.shape, i)
                 sequence.append(img_arr)  
             
-            sequence = np.stack(sequence, axis=0)     # creates a 4 dimensional numpy array from the python list containing img_arr 
+            # sequence = np.stack(sequence, axis=0)     # creates a 4 dimensional numpy array from the python list containing img_arr 
             dataset.append(sequence) 
         else:
             print("Not all images were found in this sequence.")
         
         print("\n")
 
-    dataset = np.stack(dataset, axis=0)     # creates a 5 dimensional numpy array from the python list of arrays
+    # dataset = np.stack(dataset, axis=0)     # creates a 5 dimensional numpy array from the python list of arrays
 
-    print(dataset.shape)
+    print(list_shape(dataset, 5))
 
     return dataset
 
@@ -108,10 +128,18 @@ def load_dataset(path, sequence_size, start_index, time_delta):
 # train_dataset: Dataset for training.
 # val_dataset: Dataset for validation.
 def split_dataset(dataset, split_procentage):
-    indexes = np.arange(len(dataset))
-    np.random.shuffle(indexes)
+    train_dataset = []
+    val_dataset = []
+
+    indexes = list(range(len(dataset)))
+    random.shuffle(indexes)
     train_indexes, val_indexes = indexes[: int((len(indexes)*split_procentage))], indexes[int((len(indexes)*split_procentage)) :]
-    train_dataset, val_dataset = dataset[train_indexes], dataset[val_indexes]
+    
+    for i in train_indexes:
+        train_dataset.append(dataset[i])
+
+    for i in val_indexes:
+        val_dataset.append(dataset[i])
 
     return train_dataset, val_dataset
 
@@ -122,11 +150,19 @@ def split_dataset(dataset, split_procentage):
 # data: Array containing the training data.
 #
 # Returns: 
-# x: Array containing frames 0 thourgh n-1, where n is the total number of frames in each sequence.
-# y: Array containing frames 1 thourgh n, where n is the total number of frames in each sequence.
+# x: Array containing frames 0 through n-1, where n is the total number of frames in each sequence.
+# y: Array containing frames 1 through n, where n is the total number of frames in each sequence.
 def shift_frames(data):
-    x = data[:, 0 : data.shape[1] - 1, :, :]
-    y = data[:, 1 : data.shape[1], :, :]
+    x = copy.deepcopy(data)
+    y = copy.deepcopy(data)
+
+    for i in range(len(data)):
+        x[i].pop(-1)
+
+    for i in range(len(data)):
+        y[i].pop(0)
+        print(list_shape(y, 5))
+
     return x, y
 
 
@@ -139,7 +175,7 @@ def shift_frames(data):
 # model: Model object ready for training.
 def construct_model(x_train):
     # Construct the input layer with no definite frame size.
-    inp = layers.Input(shape=(None, *x_train.shape[2:]))
+    inp = layers.Input(shape=(None, *(list_shape(x_train, 5)[2:])))
 
     # We will construct 3 `ConvLSTM2D` layers with batch normalization,
     # followed by a `Conv3D` layer for the spatiotemporal outputs.
@@ -219,9 +255,9 @@ def save_model(model, path, name, dataset_shape, general_model_info, specific_mo
         f.write(f"{general_model_info}Dataset shape: {dataset_shape}\n{specific_model_info}")
 
 
-#
+#  ===========================================================================
 # PREPARE DATASET
-#
+#  ===========================================================================
 
 # Define modifiable training hyperparameters.
 epochs = 20
@@ -236,29 +272,34 @@ dataset = load_dataset("../satellite_imagery_download/images/images", 10, 0, 15)
 
 train_dataset, val_dataset = split_dataset(dataset, 0.9)
 
-
-# Normalize the data to the 0-1 range.
-train_dataset = train_dataset / 255
-val_dataset = val_dataset / 255
-
 # Apply the processing function to the datasets.
 x_train, y_train = shift_frames(train_dataset)
 x_val, y_val = shift_frames(val_dataset)
 
 # Inspect the dataset.
-print("Training Dataset Shapes: " + str(x_train.shape) + ", " + str(y_train.shape))
-print("Validation Dataset Shapes: " + str(x_val.shape) + ", " + str(y_val.shape))
+print("Training Dataset Shapes: " + str(list_shape(x_train, 5)) + ", " + str(list_shape(y_train, 5)))
+print("Validation Dataset Shapes: " + str(list_shape(x_val, 5)) + ", " + str(list_shape(y_val, 5)))
 
 
-#
+#  ===========================================================================
 #  MODEL TRAINING
-#
+#  ===========================================================================
 
 if input("Should training continue on last checkpoint? (y/n) ") == "n":
     model = construct_model(x_train)
 else:
     model = load_model_for_training("../models/checkpoints")
-    
+
+print("Starting convertion to numpy array")
+x_train = np.array(x_train, dtype=np.float16)
+print("x_train conversion completed")  
+y_train = np.array(y_train, dtype=np.float16)
+print("y_train conversion completed")  
+x_val = np.array(x_val, dtype=np.float16)   
+print("x_val conversion completed")  
+y_val = np.array(y_val, dtype=np.float16)
+print("y_val conversion completed")
+print("Pre-processing completed")
 
 # Define some callbacks to improve training.
 early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
@@ -279,9 +320,9 @@ model.fit(
     y_train,
     batch_size=batch_size,
     epochs=epochs,
-    steps_per_epoch=None,       # default is amount of sequences divided with epochs
+    steps_per_epoch=100,       # default is amount of sequences divided with epochs
     validation_data=(x_val, y_val),
     callbacks=[early_stopping, reduce_lr, model_checkpoint]
 )
 
-save_model(model, "../models", model_name, dataset.shape, general_model_info, specific_model_info)
+save_model(model, "../models", model_name, list_shape(dataset, 5), general_model_info, specific_model_info)
