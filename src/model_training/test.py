added correct scaling of ranges for input and output images

src/model_training/test.py
@@ -15,10 +15,14 @@ import numpy as np
 # print(model_name("../models"))
 
 
-dataset = np.array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]])
+dataset = np.array([[[0, 10], [20, 30]], [[40, 50], [60, 70]]])
 predicted_frame = np.array([[8, 9], [10, 11]])
 
-dataset = list(dataset)
-dataset.append(predicted_frame)
-dataset = np.delete(np.array(dataset), 0, axis=0)
+low = -1
+high = 1
+
+dataset = dataset / (100/(high - low)) - abs(low)
+print(dataset)
+
+dataset = (dataset + abs(low)) * (100/(high - low))
 print(dataset)

src/model_training/training.py
@@ -66,14 +66,14 @@ def list_shape(arr):
 # Returns:
 # image_cache[path]: The image array associated with the path. 
 # image_cache: The dictionary containing all image arrays.
-def get_image(path, image_cache):
+def get_image(path, image_cache, low, high):
     if path not in image_cache:
         img = load_img(path)
         img = img.convert("L")
 
         # Change based on activation functions, range of input must match activation function. (sigmoid: 0 to 1. relu: 0 to infinty. tanh -1 to 1)
         image_cache[path] = np.float16(
-            (img_to_array(img, dtype=np.uint8) / 127.5) - 1.0
+            (img_to_array(img, dtype=np.uint8)) / (255/(high - low)) - abs(low)
         )
 
     return image_cache[path] , image_cache
@@ -104,7 +104,7 @@ def missing_array():
 #
 # Returns:
 # dataset: A 5 dimensional numpy array containing all training images of shape (samples, sequence_size, height, width, channels). 
-def load_dataset(path, sequence_size, start_index, time_delta):
+def load_dataset(path, sequence_size, start_index, time_delta, low, high):
     dataset = []
     image_cache = {}
 
@@ -136,7 +136,7 @@ def load_dataset(path, sequence_size, start_index, time_delta):
             
         if not any(i in missing_imgs for i in sequence_timestamps):       # Check if all images in sequence_time_arr exist by comparing to missing_imgs
             for i in tqdm(sequence_timestamps, desc="loading images"):
-                img_arr, image_cache = get_image(f"{path}/{i.replace(':','-')}.png", image_cache)
+                img_arr, image_cache = get_image(f"{path}/{i.replace(':','-')}.png", image_cache, low, high)
                 print(list_shape(img_arr), i)
                 sequence.append(img_arr)  
             
@@ -344,8 +344,10 @@ else:
 
 train_on_checkpoint = input("Should training continue on last checkpoint? (y/n) ").lower()
 
+low = int(input("Lowest value in input img array: "))
+high = int(input("Highest value in input img array: "))
 
-dataset = load_dataset("../satellite_imagery_download/images/images", 10, 0, 15)
+dataset = load_dataset("../satellite_imagery_download/images/images", 10, 0, 15, low, high)
 
 train_dataset, val_dataset = split_dataset(dataset, 0.9)
 
@@ -358,6 +360,7 @@ print("Training Dataset Shapes: " + str(list_shape(x_train)) + ", " + str(list_s
 print("Validation Dataset Shapes: " + str(list_shape(x_val)) + ", " + str(list_shape(y_val)))
 
 if load_test == "y":
+    print(x_train[0][0])
     exit()
 
 #  ===========================================================================

src/predict.py
@@ -181,7 +181,7 @@ def download_predict_img(images_path, sequence_size):
 #
 # Returns: 
 # dataset: A 4 dimensional numpy array containing each image as arrays. 
-def load_dataset(path):
+def load_dataset(path, low, high):
     dataset = []
     samples = os.listdir(f"{__file__[:len(__file__)-11]}{path}")
     print(samples)
@@ -189,7 +189,7 @@ def load_dataset(path):
     for e in samples:       # loops through the pictures for the next sequence.
         img = load_img(f"{__file__[:len(__file__)-11]}{path}/{e}")     # loads images as a PIL image
         img = img.convert("L")      # Converts images to "true gray-scale" (1 channel)
-        img_arr = img_to_array(img)     # converts images to numpy arrays
+        img_arr = img_to_array(img) / (255/(high - low)) - abs(low)     # converts images to numpy arrays and changes to correct range
         print(img_arr.shape, e)
         dataset.append(img_arr)     # adds image array to python list
 
@@ -223,7 +223,7 @@ def load_model(path, name):
 #
 # Returns: 
 # void
-def save_predicted_sequence(predicted_sequence, path, name):
+def save_predicted_sequence(predicted_sequence, path, name, low, high):
     current_date = dt.datetime.now(datetime.timezone.utc)
     current_date = (current_date + dt.timedelta(minutes=(current_date.minute // 15 * 15) - current_date.minute - 15)).isoformat()[:16]
     current_date = dt.datetime.fromisoformat(current_date)
@@ -245,9 +245,9 @@ def save_predicted_sequence(predicted_sequence, path, name):
     e = 0
     for i in predicted_sequence:
         # Change this based on output layer activation function. (If sigmoid: clip between 0 & 1. If tanh: clip between -1 & 1)
-        i = np.clip(i, 0, 1.0)
+        i = np.clip(i, low, high)
         #Change this based on output layer activation function. (If sigmoid: multiply by 255. If tanh: add 1 and then multiply by 127.5)
-        i = (i * 255).round().astype(np.uint8)
+        i = ((i + abs(low)) * (255/(high - low))).round().astype(np.uint8)
         img = array_to_img(i)
         print(i)
         img = array_to_img(i)
@@ -312,10 +312,12 @@ def plot_predicted_images(dataset, predicted_sequence):
     plt.show()
 
 
+low = int(input("Lowest value in input img array: "))
+high = int(input("Highest value in input img array: "))
 
 check_perdict_img("/satellite_imagery_download/images/predict_images", 10)
 
-dataset = load_dataset("/satellite_imagery_download/images/predict_images")
+dataset = load_dataset("/satellite_imagery_download/images/predict_images", low, high)
 
 name = input("Name of desired model ")
 model = load_model("/models", name)